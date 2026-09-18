dabaad7b92d925f23fe8d4b928da231f808bf509
Mohammed <ma3393616@gmail.com>
2026-09-18 22:32:08 +0300

    Hello from Hamo

--- main.js
+++ main.js
@@ -1,141 +1,146 @@
-let stringNum = "123" ;
-console.log(Number(stringNum) + 7); //1
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-let value = 0 ;
-if(!value)
-{
-    console.log("invalid"); //2
-    
-}
-for (let i = 0; i <= 10; i++) {
-    if(i % 2 == 0) continue ; 
-    console.log(i);//3
-    
-    
-}
-let noteven = [1, 2, 3, 4, 5];
-let even = noteven.filter((ele)=>{
-    return ele % 2 == 0 ;
-})
-console.log(even); //4
-
-
-let arr =[1,2,3];
-let arr2=[4,5,6];
-arr.push(...arr2)
-console.log(arr);//5
-let day = 5;
-switch(day){
-case  1 :
-    {
-        console.log("Sunday");
-        break;        
-    }
-    case  2 :
-    {
-        console.log("Monday");
-        break;        
-    }
-    case  3 :
-    {
-        console.log("Tuesday");
-        break;        
-    }
-    case  4 :
-    {
-        console.log("Wednesday");
-        break;        
-    }
-    case  5 :
-    {
-        console.log("Thursday");
-        break;        
-    }
-    case  6 :
-    {
-        console.log("Friday");
-        break;        
-    }
-    case  7 :
-    {
-        console.log("Saturday");
-        break;        
-    }
-    default:
-    {
-        console.log("Invalid day");
-        break;        
-    }
-
-}//6
-
-let str = ["a", "ab", "abc"];
-let strlength = str.map((str)=>{
-    return str.length
-})
-console.log(strlength); //7
-
-function Check( num) //8
-{
-    if (num % 3 == 0 && num % 5 ==0) {
-        return "divisible by both"
-    }
-    return "Not divisible by both"
-}
-
-console.log(Check(45));
- let square = x => x * x ;
-console.log( square(4)); //9
-const person = {
-    name : "john",
-    age : 25
-}
-let {name , age } = person ;
-console.log(`${name} is ${age} years old`); //10
-function sum (a , ...b)
-{
-
-    for (let i = 0; i < b.length; i++) {
-        a += b[i] ; 
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+const date = new Date;
+const deleteUserHandler = (req, res) => {
+    let id ;
+    if( req.params.id ){
+         id = req.params.id ;
     }    
-    console.log(a);
-    
-}
-sum(5 , 10 , 12 , 14) //11
+        else 
+         id = req.body.id
 
-function large(arr)
-{
-    let num = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] > num ) num = arr[i];
-        
-    }
-    console.log(num);
-    
+  if (!id) {
+    return res.status(400).json({ message: 'User ID is required' });
+  }
+
+  let users = reading(filePath);
+
+  const userID = users.findIndex((user) => user.id === id);
+
+  if (userID === -1) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  users.splice(userID, 1);
+  writing(users, filePath);
+
+  return res.json({ message: 'User deleted successfully' });
+};
+
+const filePath = path.join(__dirname, 'users.json');
+
+const reading =(filePath)=>{
+   return JSON.parse(fs.readFileSync(filePath, 'utf8'));
 }
 
-function promises(){
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{
-            res("Success")
-        },3000)
+const writing=(data,filePath)=>{
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+}
+
+let users =reading(filePath);
+
+
+app.post('/POST/user',(req , res)=>{
+   let users =reading(filePath);
+    let NewUser =req.body
+    NewUser.id = date.getMilliseconds().toString()
+   
+    const IS_USER = users.find((user) => user.email === NewUser.email )
+    
+   if (IS_USER) {
+    return res.status(400).json({ message: 'Email already exists' });
+  }
+    users.push(NewUser)
+    writing(users,filePath);
+   return res.status(201).json({
+        message : "user added successfully"
     })
+})
+app.patch('/PATCH/user/:id', (req, res) => {
+const { id } = req.params;
+const { name, age, email } = req.body;
+let users =reading(filePath);
+const userID = users.findIndex((user) => user.id === id);
+if (userID === -1) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+if (email) {
+    const emailExists = users.find((user) => user.email === email);
+    if (emailExists) {
+      return res.status(400).json({ message: 'Email already in use' });
+    }
+    users[userID].email = email;
 }
-promises().then((mes)=>{console.log(mes);
+if (name) users[userID].name = name;
+if (age) users[userID].age = age;
+writing(users,filePath)
+res.json({ message: 'User updated successfully'});
+})
+app.delete('/DELETE/user/:id', deleteUserHandler);
+app.delete('/DELETE/user', deleteUserHandler);
+
+app.get('/GET/user/getByName', (req, res) => {
+  const { name } = req.query;
+
+  if (!name) {
+    return res.status(400).json({ message: 'Name query parameter is required' });
+  }
+
+  let users = reading(filePath);
+
+  const user = users.find((user) => user.name === name);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  return res.json({ message: 'User retrieved successfully' ,users});
+});
+app.get('/GET/user', (req, res) => {
+  let users = reading(filePath);
+
+  return res.json({
+    message: 'Users retrieved successfully',
+    users
+  });
+});
+app.get('/GET/user/filter', (req, res) => {
+  const { minAge } = req.query;
+
+  if (!minAge) {
+    return res.status(400).json({ message: 'minAge query parameter is required' });
+  }
+
+  let users = reading(filePath);
+
+  const filteredUsers = users.filter((user) => user.age) >= minAge;
+
+  return res.json({
+    message: 'Users filtered successfully',users});
+});
+app.get('/GET/user/:id', (req, res) => {
+  const { id } = req.params;
+
+  let users = reading(filePath);
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  return res.json({
+    message: 'User retrieved successfully',
+    user
+  });
 });
 
-
-
-large([1,2,7,5,9]) // 13
-function keysobject (person)
-{
-    console.log(Object.keys(person));
-}
-keysobject(person); //14
-let s ="The quick brown fox";
-function split(word)
-{
-    console.log(word.split(" "));
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
     
-}
-split(s);//15
+})
